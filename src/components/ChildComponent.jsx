f2121b90476f2b17adfa6329f68d8dee71c2f9dc
hoangthanhh <thanhkuka72005@gmail.com>
2024-04-25 17:02:17 +0700

    react

---
+++ src/components/ChildComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ChildComponent = () => {
+  return <h1>This is child component</h1>;
+}
+
+export default ChildComponent;
---
+++ src/components/ParentComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ChildComponent from './ChildComponent';
+
+
+const ParentComponent = () => {
+  return (
+    <div>
+      <h2>Hello, my name is Thanh</h2>
+      <ChildComponent/>
+    </div>
+  );
+}
+
+export default ParentComponent;
